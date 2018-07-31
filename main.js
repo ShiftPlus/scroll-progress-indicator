84e4d3936625408731f834661ca07ccc363b33c6
was.li <wasuwat.se@gmail.com>
2018-07-31 11:06:18 +0700

    [💄] add gradient color

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.onscroll = function() {
-  ScrollIndicator();
-};
+  ScrollIndicator()
+}
 
 function ScrollIndicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop
 
-  var totalPageHeight = document.documentElement.scrollHeight;
-  var clientViewableHeight = document.documentElement.clientHeight;
+  var totalPageHeight = document.documentElement.scrollHeight
+  var clientViewableHeight = document.documentElement.clientHeight
 
-  var height = totalPageHeight - clientViewableHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
+  var height = totalPageHeight - clientViewableHeight
+  var scrolled = (winScroll / height) * 100
+  document.getElementById('progress-bar').style.width = scrolled + '%'
 }
